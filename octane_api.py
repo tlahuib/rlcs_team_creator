92e7a20a1a53fcc663434a2328e0d6ece3a28d50
Tlahui Bolanos <tabm96@gmail.com>
2022-11-16 09:42:56 -0600

    Api integration for matches, games, players, teams

--- octane_api.py
+++ octane_api.py
@@ -1,4 +1,3 @@
-import json
 from copy import deepcopy
 
 import requests as req
@@ -14,9 +13,7 @@ def unpaginate_api_calls(url, params, key_name, per_page=100):
         _params['page'] = page
         response = req.get(url, params=_params).json()
         results += response[key_name]
-        if response['pageSize'] < per_page:
-            print(page)
-            break
+        if response['pageSize'] < per_page: break
         page += 1
 
     return results
@@ -25,8 +22,17 @@ def unpaginate_api_calls(url, params, key_name, per_page=100):
 def get_events(**kwargs):
     return unpaginate_api_calls('https://zsr.octane.gg/events', kwargs, 'events')
 
+def get_matches(**kwargs):
+    return unpaginate_api_calls('https://zsr.octane.gg/matches', kwargs, 'matches')
 
-if __name__ == '__main__':
-    events = get_events(name='RLCS')
-    with open('test_outputs/api_response.json', 'w') as f:
-        json.dump(events, f, indent=4)
+def get_games(**kwargs):
+    return unpaginate_api_calls('https://zsr.octane.gg/games', kwargs, 'games')
+
+def get_players(**kwargs):
+    return unpaginate_api_calls('https://zsr.octane.gg/players', kwargs, 'players')
+
+def get_teams(**kwargs):
+    return unpaginate_api_calls('https://zsr.octane.gg/teams', kwargs, 'teams')
+
+def get_active_teams(**kwargs):
+    return unpaginate_api_calls('https://zsr.octane.gg/teams/active', kwargs, 'teams')
