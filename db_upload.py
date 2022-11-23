acf49d37ba4c4ab5f94cdb6c40ffc6c1573c634d
Tlahui Bolanos <tabm96@gmail.com>
2022-11-23 13:43:20 -0600

    Generalized formatting.

--- db_upload.py
+++ db_upload.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from os import getenv
 import requests as req
+import json
 
 import pandas as pd
 import sqlalchemy as db
@@ -13,8 +14,23 @@ load_dotenv()
 # DB connection
 cnx = db.create_engine(f"postgresql://{getenv('USER')}:{getenv('PASS')}@{getenv('HOST')}:{getenv('PORT')}/{getenv('DB')}")
 
+# Load colnames dict
+with open('api_columns.json') as f:
+    api_columns = json.load(f)
+
+# General format function
+def format_df(df: pd.DataFrame, df_name: str):
+    _df = df.copy()
+    col_dict = api_columns[df_name]
+    for col in df.columns:
+        if col not in set(col_dict.keys()):
+            _df.drop(columns=[col], inplace=True)
+        else:
+            _df.rename(columns={col: col_dict[col].lower()}, inplace=True)
+    return _df
+
 # General page loop
-def load_pages(url, key_name, format_function, per_page=100, params={}):
+def load_pages(url, key_name, format_function, per_page=500, params={}):
     print(f'Loading data into db for {url}...')
     _params = deepcopy(params)
     _params['perPage'] = per_page
@@ -23,58 +39,18 @@ def load_pages(url, key_name, format_function, per_page=100, params={}):
     while True:
         _params['page'] = page
         response = req.get(url, params=_params).json()
-        format_function(pd.DataFrame(response[key_name]))
+        format_function(response[key_name])
         if response['pageSize'] < per_page: break
         page += 1
 
 
 # Data-loading functions
-def load_events(df):
-    # Event data
-    events = df.copy()
-    events['startDate'] = pd.to_datetime(events['startDate'])
-    events['endDate'] = pd.to_datetime(events['endDate'])
-    events = pd.concat([events, events['prize'].apply(pd.Series)], axis=1)
-    events.rename(
-        columns={
-            '_id': 'id',
-            'startDate': 'start_date',
-            'endDate': 'end_date',
-            'amount': 'prize_amount',
-            'currency': 'prize_currency'
-            },
-        inplace=True)
+def load_events(_dict):
+    events = pd.json_normalize(_dict, sep='_')
+    stages = pd.json_normalize(_dict, sep='_', record_path='stages', meta='_id', meta_prefix='event')
 
-    # Parse stages data
-    stages = events[['id', 'stages']].rename(columns={'id': 'event_id'})
-    stages = stages.explode('stages')
-    stages = pd.concat([stages['event_id'], stages['stages'].apply(pd.Series)], axis=1)
-    stages['startDate'] = pd.to_datetime(stages['startDate'])
-    stages['endDate'] = pd.to_datetime(stages['endDate'])
-    stages = pd.concat([stages, stages['prize'].apply(pd.Series)], axis=1)
-    if 'location' in set(stages.columns):
-        stages = pd.concat([stages, stages['location'].apply(pd.Series)], axis=1)
-    stages.rename(
-        columns={
-            '_id': 'id',
-            'startDate': 'start_date',
-            'endDate': 'end_date',
-            'amount': 'prize_amount',
-            'currency': 'prize_currency'
-        },
-        inplace=True)
-    
-
-    # Clean columns
-    events_drop_cols = ['stages', 'prize', 0]
-    for col in events_drop_cols:
-        if col in set(events.columns):
-            events.drop(columns=[col], inplace=True)
-
-    stages_drop_cols = ['substages', 'location', 'prize', 0]
-    for col in stages_drop_cols:
-        if col in set(stages.columns):
-            stages.drop(columns=[col], inplace=True)
+    events = format_df(events, 'events')
+    stages = format_df(stages, 'stages')
 
     # Upload to db
     events.to_sql(name='events', con=cnx, schema='rocket_league', if_exists='append', index=False, method='multi')
@@ -84,16 +60,21 @@ def load_events(df):
     return events, stages
 
 
-def load_matches():
-    matches = pd.read_json('test/outputs/api_matches.json')
+def load_matches(_dict):
+    matches = pd.json_normalize(_dict, sep='_')
+    
+    matches = format_df(matches, 'matches')
+
+    # Upload to db
+    matches.to_sql(name='matches', con=cnx, schema='rocket_league', if_exists='append', index=False, method='multi')
+    print(f'\tLoaded {matches.__len__()} rows to matches.')
 
     return matches
 
 
 if __name__ == '__main__':
-    # Load events
-    load_pages('https://zsr.octane.gg/events', 'events', load_events)
+    # # Load events
+    # load_pages('https://zsr.octane.gg/events', 'events', load_events)
 
-    # matches = load_matches()
-    # print(matches)
-    # print(matches.info())
+    # Load matches
+    load_pages('https://zsr.octane.gg/matches', 'matches', load_matches)
