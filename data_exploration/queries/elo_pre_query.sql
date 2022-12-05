8c2ad6cd09f93c849581a9a7da4839a3d4d34a21
Tlahui Bolanos <tabm96@gmail.com>
2022-12-05 11:19:27 -0600

    Removed team rosters with only 1 event

--- data_exploration/queries/elo_pre_query.sql
+++ data_exploration/queries/elo_pre_query.sql
@@ -1,14 +1,53 @@
 -- melt to add win/lose col for better team control
 with game_teams as (
-	select
-		gp.game_id,
+	select 
+		gp.event_id,
 		gp.team_id,
-		array_agg(gp.id) as players
+		gp.game_id,
+		array_agg(distinct gp.id) as players
 	from rocket_league.games_players gp
+	left join rocket_league.events e 
+		on e.id = gp.event_id
 	where
-		not coalesce((gp.coach or gp.substitute), false)
-		and team_id is not null
+		e.mode = 3
+		and not coalesce((gp.coach or gp.substitute), false)
+		and gp.team_id is not null
+	group by 1, 2, 3
+), game_count as (
+	select
+		gt.event_id,
+		gt.team_id,
+		gt.players,
+		count(game_id) as n_games
+	from game_teams gt
+	where
+		array_length(gt.players, 1) = 3
+	group by 1, 2, 3
+), game_count_ordered as (
+	select
+		*,
+		row_number() over(partition by event_id, team_id order by n_games desc) as _row
+	from game_count gc
+), event_count as (
+	select distinct
+		gco.team_id,
+		gco.players,
+		count(distinct gco.event_id) n_events
+	from game_count_ordered gco
+	where
+		gco._row = 1
 	group by 1, 2
+), valid_games as (
+	select
+		gt.game_id,
+		count(*) = 2 is_valid
+	from event_count ec
+	left join game_teams gt
+		on gt.team_id = ec.team_id
+		and gt.players = ec.players
+	where
+		ec.n_events > 1
+	group by 1
 )
 select
 	g.date,
@@ -37,14 +76,11 @@ left join game_teams bgt
 left join game_teams ogt
 	on g.id = ogt.game_id 
 	and g.orange_team_id = ogt.team_id
+left join valid_games vg
+	on vg.game_id = g.id
 where
-	e.mode = 3
-	and not (g.blue_winner is null and g.orange_winner is null)
+	not (g.blue_winner is null and g.orange_winner is null)
 	and not g.date is null
+	and vg.is_valid
 order by 
 	g.date
-
-
-
-
-
