b52dd6a484bf77199ef98de17d3d339272885e9f
Tlahui Bolanos <tabm96@gmail.com>
2022-12-14 10:27:55 -0600

    Added db incremental loading

--- db_setup/db_upload.py
+++ db_setup/db_upload.py
@@ -17,9 +17,9 @@ cnx = db.create_engine(
 with open('db_setup/api_columns.json') as f:
     api_columns = json.load(f)
 
+table_dates = {'events': 'start_date', 'matches': 'date', 'games': 'date'}
+
 # General format function
-
-
 def format_df(df: pd.DataFrame, df_name: str):
     _df = df.copy()
     col_dict = api_columns[df_name]
@@ -35,34 +35,50 @@ def format_df(df: pd.DataFrame, df_name: str):
 # General page loop
 
 
-def load_pages(url, key_name, format_function, per_page=500, params={}):
+def load_pages(url:str , key_name: str, format_function, params={'perPage': 500}):
     print(f'Loading data into db for {url}...')
     _params = deepcopy(params)
-    _params['perPage'] = per_page
+    if key_name in {'events', 'matches', 'games'}:
+        last_date = cnx.execute(f'select max(updated_at) from rocket_league.{key_name}').fetchone()[0]
+        _params['after'] = str(last_date.date())
     page = 1
 
     while True:
         _params['page'] = page
         response = req.get(url, params=_params).json()
-        format_function(response[key_name])
-        if response['pageSize'] < per_page:
+        n_added = format_function(response[key_name])
+        print(page, _params['perPage'], response['pageSize'], n_added)
+        if response['pageSize'] < _params['perPage']:
             break
         page += 1
 
 
+def get_new_records(api_df: pd.DataFrame, table: str, col: str = 'id'):
+    db_df = pd.read_sql(f'select {col} from rocket_league.{table}', cnx)
+    loaded_records = set(db_df[col])
+    api_records = set(api_df[col])
+    new_records = api_records - loaded_records
+    return api_df[api_df[col].apply(lambda x: x in new_records)]
+
+
 # Data-loading functions
-def load_teams(_dict):
+def load_teams(_dict: dict):
     # Format teams data
     teams = pd.json_normalize(_dict, sep='_')
 
     teams = format_df(teams, 'teams')
 
-    # Upload to db
-    teams.to_sql(name='teams', con=cnx, schema='rocket_league',
-                 if_exists='append', index=False, method='multi')
-    print(f'\tLoaded {teams.__len__()} rows to teams.')
+    # Check for new teams
+    teams = get_new_records(teams, 'teams')
 
-    return teams
+
+    # Upload to db
+    if not teams.empty:
+        teams.to_sql(name='teams', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+        print(f'\tLoaded {teams.__len__()} rows to teams.')
+
+    return teams.__len__()
 
 
 def load_players(_dict):
@@ -78,14 +94,22 @@ def load_players(_dict):
     accounts = format_df(accounts,'players_accounts')
     accounts.dropna(inplace=True)
 
+    # Check for new players
+    players = get_new_records(players, 'players')
+    accounts = get_new_records(accounts, 'players_accounts', 'platform_id')
+
     # Upload to db
-    players.to_sql(name='players', con=cnx, schema='rocket_league',
-                 if_exists='append', index=False, method='multi')
-    accounts.to_sql(name='players_accounts', con=cnx, schema='rocket_league',
-                 if_exists='append', index=False, method='multi')
+    if not players.empty:
+        players.to_sql(name='players', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
+    if not accounts.empty:
+        accounts.to_sql(name='players_accounts', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
     print(f'\tLoaded {players.__len__()} rows to players and {accounts.__len__()} to players_accounts.')
 
-    return players, accounts
+    return players.__len__(), accounts.__len__()
 
 
 def load_events(_dict):
@@ -95,15 +119,22 @@ def load_events(_dict):
     events = format_df(events, 'events')
     stages = format_df(stages, 'stages')
 
-    # Upload to db
-    events.to_sql(name='events', con=cnx, schema='rocket_league',
-                  if_exists='append', index=False, method='multi')
-    stages.to_sql(name='events_stages', con=cnx, schema='rocket_league',
-                  if_exists='append', index=False, method='multi')
-    print(
-        f'\tLoaded {events.__len__()} rows to events and {stages.__len__()} rows to stages.')
+    # Check for new events
+    events = get_new_records(events, 'events')
+    stages = get_new_records(stages, 'events_stages', 'event_id')
 
-    return events, stages
+    # Upload to db
+    if not events.empty:
+        events.to_sql(name='events', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
+    if not stages.empty:
+        stages.to_sql(name='events_stages', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+    
+    print(f'\tLoaded {events.__len__()} rows to events and {stages.__len__()} rows to stages.')
+
+    return events.__len__(), stages.__len__()
 
 
 def load_matches(_dict):
@@ -156,15 +187,22 @@ def load_matches(_dict):
     players = format_df(players, 'matches_players')
     players.drop_duplicates(subset=['match_id', 'id'], inplace=True)
 
-    # Upload to db
-    matches.to_sql(name='matches', con=cnx, schema='rocket_league',
-                   if_exists='append', index=False, method='multi')
-    players.to_sql(name='matches_players', con=cnx, schema='rocket_league',
-                   if_exists='append', index=False, method='multi')
-    print(
-        f'\tLoaded {matches.__len__()} rows to matches and {players.__len__()} rows to matches_players.')
+    # Check for new matches
+    matches = get_new_records(matches, 'matches')
+    players = get_new_records(players, 'matches_players', 'match_id')
 
-    return matches, players
+    # Upload to db
+    if not matches.empty:
+        matches.to_sql(name='matches', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
+    if not players.empty:
+        players.to_sql(name='matches_players', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
+    print(f'\tLoaded {matches.__len__()} rows to matches and {players.__len__()} rows to matches_players.')
+
+    return matches.__len__(), players.__len__()
 
 
 def load_games(_dict):
@@ -217,17 +255,26 @@ def load_games(_dict):
     players = format_df(players, 'games_players')
     players.drop_duplicates(subset=['game_id', 'id'], inplace=True)
 
+    #  Check for new games
+    games = get_new_records(games, 'games')
+    players = get_new_records(players, 'games_players', 'game_id')
+
     # Upload to db
-    games.to_sql(name='games', con=cnx, schema='rocket_league',
-                 if_exists='append', index=False, method='multi')
-    players.to_sql(name='games_players', con=cnx, schema='rocket_league',
-                   if_exists='append', index=False, method='multi')
+    if not games.empty:
+        games.to_sql(name='games', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
+    if not players.empty:
+        players.to_sql(name='games_players', con=cnx, schema='rocket_league',
+                    if_exists='append', index=False, method='multi')
+
     print(f'\tLoaded {games.__len__()} rows to games and {players.__len__()} rows to games_players.')
 
-    return games, players
+    return games.__len__(), players.__len__()
 
 
 if __name__ == '__main__':
+    pass
     # # print(json.dumps(api_columns, indent=4))
 
     # # Load teams
@@ -239,8 +286,9 @@ if __name__ == '__main__':
     # # Load events
     # load_pages('https://zsr.octane.gg/events', 'events', load_events)
 
-    # Load matches
-    load_pages('https://zsr.octane.gg/matches', 'matches', load_matches)
+    # # Load matches
+    # load_pages('https://zsr.octane.gg/matches', 'matches', load_matches)
 
-    # Load games
-    load_pages('https://zsr.octane.gg/games', 'games', load_games)
+    # # Load games
+    # load_pages('https://zsr.octane.gg/games', 'games', load_games)
+
