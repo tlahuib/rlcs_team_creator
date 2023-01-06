d5ee4d8fb839f7255ae9588046f75994b16ad72d
Tlahui Bolanos <tabm96@gmail.com>
2023-01-06 12:22:39 -0600

    Modified stability definiiton

--- db_setup/views_setup.sql
+++ db_setup/views_setup.sql
@@ -1,116 +1,132 @@
+-- View that gets the first appearance of each team's roster
+drop view rocket_league.rosters;
+create or replace view rocket_league.rosters as
+with grouped_games_players as (
+	select
+		gp.game_id,
+		gp.team_id,
+		gp.color,
+		string_agg(distinct gp.id, ',' order by gp.id) as team_players,
+		string_agg(distinct p.tag, ',' order by p.tag) as team_players_names
+	from rocket_league.games_players gp
+	left join rocket_league.players p 
+		on p.id = gp.id
+	group by 1, 2, 3
+), games_results as (
+	select
+		g.date,
+		ggp.*,
+		case
+			when ggp.color = 'blue' and g.blue_winner then 1
+			when ggp.color = 'orange' and g.orange_winner then 1
+			else 0
+		end as "result"
+	from grouped_games_players ggp
+	left join rocket_league.games g
+		on g.id = ggp.game_id
+	where
+		g.date is not null
+), grouped_games_results as (
+	select
+		gr.team_id,
+		gr.team_players,
+		gr.team_players_names,
+		count(gr.result) as total_games,
+		sum(gr.result) as won_games,
+		count(gr.result) - sum(gr.result) as lost_games,
+		min(gr.date) as first_appearance,
+		max(gr.date) as last_appearance
+	from games_results gr
+	group by 1, 2, 3
+)
+select 
+	ggr.team_id,
+	t.name as team_name,
+	row_number() over(partition by ggr.team_id order by ggr.first_appearance) - 1 as roster_id,
+	ggr.team_players,
+	ggr.team_players_names,
+	ggr.total_games,
+	ggr.won_games,
+	ggr.lost_games,
+	ggr.first_appearance,
+	ggr.last_appearance
+from grouped_games_results ggr
+left join rocket_league.teams t 
+	on t.id = ggr.team_id
+order by 
+	2, 9
+
+
 -- View that contains the games in format to calculate Elo ratings
-create or replace materialized view rocket_league.pre_elo_games as 
-with game_teams as (
+drop materialized view rocket_league.pre_elo_games;
+create materialized view rocket_league.pre_elo_games as 
+with games_results as (
+	select
+		coalesce(g.date, m.date + concat(g.number * 5, ' minutes')::interval) as date,
+		g.id as game_id,
+		case
+			when g.blue_winner then coalesce(g.blue_team_id, m.blue_team_id)
+			when g.orange_winner then coalesce(g.orange_team_id , m.orange_team_id)
+		end as winner_team_id,
+		case
+			when g.blue_winner then coalesce(g.orange_team_id , m.orange_team_id) 
+			when g.orange_winner then coalesce(g.blue_team_id, m.blue_team_id)
+		end as loser_team_id
+	from rocket_league.games g
+	left join rocket_league.events e 
+		on e.id = g.event_id
+	left join rocket_league.matches m 
+		on m.id = g.match_id
+	where
+		e.mode = 3
+		and not(g.orange_winner is null and g.blue_winner is null)
+), grouped_games_players as (
 	select 
-		gp.event_id,
 		gp.team_id,
 		gp.game_id,
-		array_agg(distinct gp.id) as players
+		string_agg(distinct gp.id, ',' order by gp.id) as team_players,
+		count(*) as n_players
 	from rocket_league.games_players gp
 	left join rocket_league.events e 
 		on e.id = gp.event_id
 	where
-		e.mode = 3
-		and not coalesce((gp.coach or gp.substitute), false)
+		not coalesce((gp.coach or gp.substitute), false)
 		and gp.team_id is not null
-	group by 1, 2, 3
-), game_count as (
-	select
-		gt.event_id,
-		gt.team_id,
-		gt.players,
-		count(game_id) as n_games
-	from game_teams gt
-	where
-		array_length(gt.players, 1) = 3
-	group by 1, 2, 3
-), game_count_ordered as (
-	select
-		*,
-		row_number() over(partition by event_id, team_id order by n_games desc) as _row
-	from game_count gc
-), event_count as (
-	select distinct
-		gco.team_id,
-		gco.players,
-		count(distinct gco.event_id) n_events
-	from game_count_ordered gco
-	where
-		gco._row = 1
 	group by 1, 2
-), valid_games as (
-	select
-		gt.game_id,
-		count(*) = 2 is_valid
-	from event_count ec
-	left join game_teams gt
-		on gt.team_id = ec.team_id
-		and gt.players = ec.players
-	where
-		ec.n_events > 1
-	group by 1
 )
 select
-	g.date,
-	case
-		when g.blue_winner then g.blue_team_id
-		when g.orange_winner then g.orange_team_id
-	end as winner_team_id,
-	case
-		when g.blue_winner then bgt.players
-		when g.orange_winner then ogt.players
-	end as winner_team_players,
-	case
-		when g.blue_winner then g.orange_team_id
-		when g.orange_winner then g.blue_team_id
-	end as loser_team_id,
-	case
-		when g.blue_winner then ogt.players
-		when g.orange_winner then bgt.players
-	end as loser_team_players
-from rocket_league.events e 
-left join rocket_league.games g
-	on e.id = g.event_id
-left join game_teams bgt
-	on g.id = bgt.game_id 
-	and g.blue_team_id = bgt.team_id
-left join game_teams ogt
-	on g.id = ogt.game_id 
-	and g.orange_team_id = ogt.team_id
-left join valid_games vg
-	on vg.game_id = g.id
-where
-	not (g.blue_winner is null and g.orange_winner is null)
-	and not g.date is null
-	and vg.is_valid
-order by 
-	g.date
-	
+	gr.date,
+	gr.game_id,
+	gr.winner_team_id,
+	wt.name as winner_team_name,
+	wr.roster_id as winner_roster_id,
+	wp.team_players as winner_team_players,
+	gr.loser_team_id,
+	lt.name as loser_team_name,
+	lr.roster_id as loser_roster_id,
+	lp.team_players as loser_team_players
+from games_results gr
+-- Get team players
+left join grouped_games_players wp 
+	on wp.game_id = gr.game_id
+	and wp.team_id = gr.winner_team_id
+left join grouped_games_players lp 
+	on lp.game_id = gr.game_id
+	and lp.team_id = gr.loser_team_id
+-- Get team names
+left join rocket_league.teams wt 
+	on wt.id = gr.winner_team_id
+left join rocket_league.teams lt 
+	on lt.id = gr.loser_team_id
+-- Get team rosters
+left join rocket_league.rosters wr
+	on wr.team_id = gr.winner_team_id
+	and wr.team_players = wp.team_players
+left join rocket_league.rosters lr
+	on lr.team_id = gr.loser_team_id
+	and lr.team_players = lp.team_players
+order by 1
 
--- View that gets the first appearance of each team's roster
-create or replace view rocket_league.team_dates as
-with pre_team_dates as (
-	select
-		peg.winner_team_id as team_id,
-		peg.winner_team_players as team_players,
-		min(peg.date) as min_date,
-		max(peg.date) as max_date
-	from rocket_league.pre_elo_games peg
-	group by 1, 2
-	union all
-	select
-		peg.loser_team_id as team_id,
-		peg.loser_team_players as team_players,
-		min(peg.date) as min_date,
-		max(peg.date) as max_date
-	from rocket_league.pre_elo_games peg
-	group by 1, 2
-)
-select
-	team_id,
-	team_players,
-	min(min_date) as first_appearance,
-	max(max_date) as last_appearance
-from pre_team_dates ptd
-group by 1, 2
-order by 1, 3
+
+
+	
